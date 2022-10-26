64fa7472e5521c8119fa1af837d6b22dde76406a
tommycheung1989 <tommycheung1989@gmail.com>
2022-10-27 01:16:02 +0800

    add keyboard and location

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard, Keyboard } from "grammy";
 import { Menu } from "@grammyjs/menu";
 
 import dotenv from 'dotenv';
@@ -9,22 +9,32 @@ dotenv.config();
 const token = process.env.API_TOKEN as string;
 
 const bot = new Bot(token);
-const gemBox = new GemBox(100);
-
+// const gemBox = new GemBox(100);
+const keyboard = new Keyboard().text('location').row()
 const menu = new Menu("mainMenu")
-  .text("report", (ctx) => ctx.reply(gemBox.getReport())).row()
-    .text("add", (ctx) => {
-        gemBox.addGem(1);
-        ctx.reply('gem was added.')}).row()
-    .text("remove", (ctx) => {
-        gemBox.remove(1)
-        ctx.reply('gem was removed');
-    }).row()
+  .text("hi", (ctx) => ctx.reply('Hi Jessi!')).row()
+  .text("who am i", (ctx) => ctx.reply(`i don't know`)).row()
+    // .text("add", (ctx) => {
+    //     gemBox.addGem(1);
+    //     ctx.reply('gem was added.')}).row()
+    // .text("remove", (ctx) => {
+    //     gemBox.remove(1)
+    //     ctx.reply('gem was removed');
+    // }).row()
 
 bot.use(menu);
 
+bot.on('msg:text', ctx => {
+  if (ctx.message!.text === 'location') {
+    console.log(ctx.message?.location)
+    ctx.reply('give me your location!', { reply_markup: keyboard })
+  }
+})
 bot.command("start", async (ctx) => {
   // Send the menu.
   await ctx.reply("Check out this menu:", { reply_markup: menu });
 });
+bot.hears('location',ctx=>console.log(ctx.msg.location))
+bot.on('message',ctx=>console.log(ctx.message))
+// bot.command()
 bot.start();
